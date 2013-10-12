6495d37f81d6a035918fd5cc47447a0d64975abc
Stratos Goudelis <sgoudelis@gmail.com>
2013-10-13 01:48:12 +0300

    changed config template

--- connections/config-template.py
+++ connections/config-template.py
@@ -1,16 +1,39 @@
+# -*- coding: UTF8 -*-
 config = {
     'btc' :  {
         'rpcusername': "testuser",
         'rpcpassword': "testnet",
-        'rpchost': "sunflower",
+        'rpchost': "localhost",
         'rpcport': "7000",
         'currency_name': 'BitCoin (BTC)',
+        'symbol': "B⃦",
+        'code': 'BTC'
     },
     'ltc':  {
         'rpcusername': "testuser",
         'rpcpassword': "testnet",
-        'rpchost': "sunflower",
+        'rpchost': "localhost",
         'rpcport': "7001",
         'currency_name': 'LiteCoin (LTC)',
+        'symbol': "Ł",
+        'code': 'LTC'
+    },
+    'nmc':  {
+        'rpcusername': "testuser",
+        'rpcpassword': "testnet",
+        'rpchost': "localhost",
+        'rpcport': "7002",
+        'currency_name': 'NameCoin (NMC)',
+        'symbol': "ℕ",
+        'code': 'NMC'
+    },
+    'ppc':  {
+        'rpcusername': "testuser",
+        'rpcpassword': "testnet",
+        'rpchost': "localhost",
+        'rpcport': "7003",
+        'currency_name': 'PPCoin (PPC)',
+        'symbol': "Ᵽ",
+        'code': 'PPC'
     },
 }
