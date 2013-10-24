16d8dd5c4da04d5d20e7e781be36845afa21fe8a
Stratos Goudelis <sgoudelis@gmail.com>
2013-10-25 00:20:01 +0300

    some minor fixes

--- accounts/views.py
+++ accounts/views.py
@@ -100,6 +100,16 @@ def details(request, account_address="pipes"):
     if account:
         # get transaction details
         transactions = generic.getTransactionsByAccount(connector, account['name'], account['currency'], reverse_order=True)
+        generic.prettyPrint(transactions)
+        for transaction in transactions:
+            transaction['currency_symbol'] = generic.getCurrencySymbol(transaction['currency'].lower())
+            if transaction.get('txid', None):
+                transaction['details'] = connector.gettransactiondetails(transaction['txid'], transaction['currency'])
+                print transaction['details']
+                if transaction['details'].get('sender_address', True):
+                    transaction['details']['sender_address'] = '(unknown)'
+            if transaction['category'] == 'move':
+                transaction['otheraccount_address'] = connector.getaddressesbyaccount(transaction['otheraccount'], transaction['currency'])
     else:
         account = {}
         account['name'] = _('no such account')
