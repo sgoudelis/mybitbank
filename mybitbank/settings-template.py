f0f2b0d38fd88ae2f0fc87618a42e7fdb2d2ba8a
Stratos Goudelis <sgoudelis@gmail.com>
2014-06-10 11:29:12 +0300

    changed default database backend

--- mybitbank/settings-template.py
+++ mybitbank/settings-template.py
@@ -11,8 +11,8 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',  # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'mybitbank',  # Or path to database file if using sqlite3.
+        'ENGINE': 'django.db.backends.sqlite3',  # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'mybitbank.sqlite3',  # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
         'USER': 'mybitbank',
         'PASSWORD': 'mybitbank',
